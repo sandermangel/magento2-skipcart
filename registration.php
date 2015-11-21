64b6b2dcfd85ab030387706ce15a48e9fd58950b
Sander Mangel <mail.contentmedia@gmail.com>
2015-11-21 15:39:58 +0100

    Sama Skipcart - version 0.1.0

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Sama_Skipcart extension
+ *
+ * @package   Sama_Skipcart
+ * @copyright 2015 Sander Mangel
+ * @license   OSL-3.0 - See LICENSE.md for license details.
+ * @author    Sander Mangel <sander@sandermangel.nl>
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Sama_Skipcart',
+    __DIR__
+);
